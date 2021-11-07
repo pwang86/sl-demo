99044c3bbdcab1d59586a036cad7d7f0fbb64082
pwang86 <pwang86@gmail.com>
2021-11-07 23:18:54 +1100

    json file in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,44 @@ const clickMobileBurgerMenu = () => {
         myMenu.classList.remove("visible")
     }
 };
+    
+let jsonRecord = `[
+    {
+        "model": "WR841N",
+        "location": "A0C1",
+        "version": "14.0",
+        "date": "2021-11-11",
+        "isTPG": "false",
+        "isNZ": "false",
+        "quantity": "100"
+    },
+    {
+        "model": "WA850RE",
+        "location": "A0C2",
+        "version": "6.0",
+        "date": "2021-11-11",
+        "isTPG": "false",
+        "isNZ": "false",
+        "quantity": "120"
+    },
+    {
+        "model": "M5 (2P)",
+        "location": "A0D1",
+        "version": "6.0",
+        "date": "2021-11-11",
+        "isTPG": "false",
+        "isNZ": "false",
+        "quantity": "120"
+    },
+    {
+        "model": "VR300",
+        "location": "A0D2",
+        "version": "3.20",
+        "date": "2021-11-11",
+        "isTPG": "false",
+        "isNZ": "false",
+        "quantity": "180"
+    }
+]`;
+const record = JSON.parse(jsonRecord);
+console.log(record);
